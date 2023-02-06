28c16fbf8057886490020ab0a29d5337c0f60c48
“saka-git <sakamoto.tatsuya1997@gmail.com>
2023-02-06 17:24:57 +0900

    カルーセル実装

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  //カルーセル
+  $(".carousel").slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    fade: true,
+    swipe: true,
+  });
+});
